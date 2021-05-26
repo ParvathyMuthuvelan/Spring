da9cb6693f4857a430cef154d967203193b03e8e
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-05-26 17:47:49 +0530

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,19 +1,27 @@
-package com.beans;
+package com.example.restservice;
 
-
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class Person {
+	@NotNull(message = "Id is required")
+	private Integer id;
+	@NotEmpty(message = "Name is required")
+	@Size(min=10,message="Name should be minimum 10 characters") 
 	private String name;
-	private Address address;
-	public Person() {}
+	@NotNull(message = "Age is required")
+	private Integer age;
 	
-	
-	public Person(String name, Address address) {
-		this.name = name;
-		this.address = address;
+	public Integer getId() {
+		return id;
 	}
-
-
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 	public String getName() {
 		return name;
 	}
@@ -21,20 +29,5 @@ public class Person {
 		this.name = name;
 	}
 
-	public Address getAddress() {
-		return address;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Person [name=%s, address=%s]", name, address);
-	}
-
 	
-    
-
 }
